7cbb7662ac05ceec34b67a9a22cd9c55eed39bf5
AugustineOchieng <gusochieng@gmail.com>
2019-04-16 09:58:12 +0300

    deployment to heroku

--- app/config.py
+++ app/config.py
@@ -1,7 +1,37 @@
+
+import os
+
 class Config:
-  NEW_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
-  ARTICLE_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    '''
+    General configuration parent class
+    '''
+    NEWS_API_SOURCE_URL='https://newsapi.org/v2/sources?apiKey={}'
+    # CAT_API_URL='https://newsapi.org/v2/everything?q={}&sortBy=relevancy&apiKey={}'
+    NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
+    CAT_API_URL='https://newsapi.org/v2/top-headlines?country=us&category={}&apiKey={}'
+
+
+
+
 class ProdConfig(Config):
-  pass
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
 class DevConfig(Config):
-  DEBUG = True
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
---
+++ start.sh
@@ -0,0 +1,3 @@
+export NEWS_API_KEY=cb74355bb75c428fb58bb3dff2fa68ab
+
+python3.6 manage.py server
