9f50e17c679c53400ea5152fc8275bfbb7e85fc1
AugustineOchieng <gusochieng@gmail.com>
2019-04-16 12:29:49 +0300

    Deployment

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export NEWS_API_KEY=cb74355bb75c428fb58bb3dff2fa68ab
-export SECRET_KEY=gus
+export NEWS_API_KEY='cb74355bb75c428fb58bb3dff2fa68ab'
+export SECRET_KEY= 'gus'
 
 python3.6 manage.py server
